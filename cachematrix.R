8f7ca0e32ba5165fad37381f4c0d085e5809b73e
24sbit060-droid <24sbit060@psgrkcw.ac.in>
2025-12-16 13:25:11 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,12 +2,8 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
 }
-
-
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
